a3713f307fbfbd53c3568b42027c17c578249bbc
Omarwahba611 <114486478+Omarwahba611@users.noreply.github.com>
2023-11-07 22:47:13 +0200

    Update LCD.h

--- LCD.h
+++ LCD.h
@@ -3,7 +3,7 @@
 
 void LCD_Init();
 void LCD_Clear();
-void LCD_String(const char *str);
+void LCD_String(char *str);
 void LCD_Command(unsigned char cmnd);
 void LCD_Char(unsigned char data);
 void updateLCD(float temperature, float upperLimit);
